9a14151b98e66d6aea69c80bad7e3fced80063e6
iankrx3 <iankrx3@gmail.com>
2026-09-02 20:30:41 +0900

    text size up

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontSize: {
+        xs: ['var(--fs-xs)', { lineHeight: '1.4' }],
+        sm: ['var(--fs-sm)', { lineHeight: '1.5' }],
+        base: ['var(--fs-base)', { lineHeight: '1.5' }],
+        lg: ['var(--fs-lg)', { lineHeight: '1.5' }],
+        xl: ['var(--fs-xl)', { lineHeight: '1.4' }],
+        '2xl': ['var(--fs-2xl)', { lineHeight: '1.3' }],
+        '3xl': ['var(--fs-3xl)', { lineHeight: '1.25' }],
+        '4xl': ['var(--fs-4xl)', { lineHeight: '1.2' }],
+      },
       colors: {
         'miyeon-main': '#5A514D',
         'miyeon-sub1': '#D49A9A',
